66d92bdab7427d0eb8d215556b7e96e589f4d21d
easso <easso1@protonmail.com>
2022-03-30 21:53:57 -0700

    Task 1

--- i-heart-react/src/App.js
+++ i-heart-react/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import Heart from './components/Heart';
+import Header from './components/Header';
 import './App.css';
 
 const message = 'cool cud';
 
+
 function App() {
-    return 
+  return (
+      <>
+          <Header />
+          <Heart msg={message} />
+      </>
+  );
 }
 
 export default App;
