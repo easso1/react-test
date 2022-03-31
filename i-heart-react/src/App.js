53289601429a11cc262425e33b009be7528dcd51
easso <easso1@protonmail.com>
2022-03-30 21:01:37 -0700

    Task 1 Assign1

--- i-heart-react/src/App.js
+++ i-heart-react/src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Heart from './components/Heart';
+import Header from './components/Header';
+
 const message = 'cool cud';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <Heart msg={message}/>;
+};
 
 export default App;
