2b36ad48354ce87a40c3f506b4685e86fba7b8d4
easso <easso1@protonmail.com>
2022-04-16 12:14:35 -0700

    Week 3 assignment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -9,7 +9,6 @@ function RenderCampsite({campsite}) {
         <Card>
           <CardImg top src={campsite.image} alt={campsite.name} />
           <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
             <CardText>{campsite.description}</CardText>
           </CardBody>
         </Card>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import 'typeface-open-sans';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
+import App from './App';
 
 
 ReactDOM.render(
